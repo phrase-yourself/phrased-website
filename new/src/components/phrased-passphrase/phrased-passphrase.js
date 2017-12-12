c6572c0d0012968c44d341d80a39cf51ceb7cee9
Jakob Holderbaum <jakob@holderbaum.io>
2017-12-12 08:48:57 +0100

    Separate stylesheets into postcss domain

--- new/src/components/phrased-passphrase/phrased-passphrase.js
+++ new/src/components/phrased-passphrase/phrased-passphrase.js
@@ -1,20 +1,8 @@
 let template = () => {
   return `
-    <style>
-      phrased-passphrase {
-        text-align: center;
-        font-size: 2em;
-      }
-
-      phrased-passphrase span {
-        border-bottom: 1px solid lightblue;
-        padding: 0.1em 0;
-      }
-    </style>
     <div class="phrased-passphrase-phrase">
-      <span>
-        zieren wirke herab plump lassen lampe stift 286
-      </span>
+      <span>zieren wirke herab plump lassen lampe stift 286</span>
+    </div>
   `
 }
 
