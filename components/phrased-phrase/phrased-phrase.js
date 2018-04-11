0ffaff9695e198e691be7419744bcfbf1c6b2e4c
Jakob Holderbaum <jakob@holderbaum.io>
2018-04-11 19:18:02 +0200

    Implement simple component structure

--- components/phrased-phrase/phrased-phrase.js
+++ components/phrased-phrase/phrased-phrase.js
@@ -1,12 +1,12 @@
 const currentScript = document.currentScript || document._currentScript
 const doc = currentScript.ownerDocument
-const templateElement = doc.getElementById('phrased-app-template')
+const templateElement = doc.getElementById('phrased-phrase-template')
 
 const template = () => {
   return document.importNode(templateElement.content, true)
 }
 
-class PhrasedApp extends window.HTMLElement {
+class PhrasedPhrase extends window.HTMLElement {
   constructor () {
     super()
     this.root = this.attachShadow({mode: 'open'})
@@ -17,4 +17,4 @@ class PhrasedApp extends window.HTMLElement {
   }
 }
 
-window.customElements.define('phrased-app', PhrasedApp)
+window.customElements.define('phrased-phrase', PhrasedPhrase)
